f18eb65e4f4f42e1e7aede0381c43807c646f915
KorolevaArina <arina-koroleva23@yandex.ru>
2026-04-27 15:01:07 +0300

    changes in Shapes.h, Rectangle.h

--- Source/Rectangle.h
+++ Source/Rectangle.h
@@ -2,17 +2,19 @@
 #include "Shapes.h"
 
 template <typename num> class Rectangle {
-  point_coor<num> top_left_;
-  point_coor<num> bottom_right_;
+    point_coor<num> top_left_;
+    point_coor<num> bottom_right_;
 
 public:
-  Rectangle(const point_coor<num> &top_left = point_coor<num>(),
-            const point_coor<num> &bottom_right = point_coor<num>())
-      : top_left_(top_left), bottom_right_(bottom_right) {}
+    Rectangle(
+        const point_coor<num>& top_left     = point_coor<num>(),
+        const point_coor<num>& bottom_right = point_coor<num>())
+        : top_left_(top_left), bottom_right_(bottom_right) {}
 
-  const point_coor<num> &top_left() const { return top_left_; }
-  const point_coor<num> &bottom_right() const { return bottom_right_; }
+    const point_coor<num>& topLeft()     const { return top_left_; }
+    const point_coor<num>& bottomRight() const { return bottom_right_; }
 
-  void set_top_left(const point_coor<num> &p) { top_left_ = p; }
-  void set_bottom_right(const point_coor<num> &p) { bottom_right_ = p; }
+    void setTopLeft(const point_coor<num>& p)     { top_left_     = p; }
+    void setBottomRight(const point_coor<num>& p) { bottom_right_ = p; }
+
 };
